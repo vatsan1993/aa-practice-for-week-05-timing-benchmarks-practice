8f2a23c7a8d9d91e236ce6a73b0d2c2d1f11414e
Srivatsan <vatsan1993@gmail.com>
2023-09-12 14:48:35 +0530

    finished bonus

--- bonus.js
+++ bonus.js
@@ -1,15 +1,15 @@
 const [addNums, addManyNums] = require('./phase-1');
 
-function addNums10Timing(increment) {
+function addNums100Timing(increment) {
   // Copy your `addNums10` code here
   // Then, add timing code
   let start = Date.now();
   let values = [];
-  for (let i = increment; i <= 10 * increment; i += increment) {
-    console.time('addNums10Timing');
+  for (let i = increment; i <= 100 * increment; i += increment) {
+    console.time('addNums100Timing');
     values.push(addNums(i));
-    console.timeLog('addNums10Timing');
-    console.timeEnd('addNums10Timing');
+    console.timeLog('addNums100Timing');
+    console.timeEnd('addNums100Timing');
   }
   let end = Date.now();
   console.log(end - start);
@@ -17,18 +17,18 @@ function addNums10Timing(increment) {
   // Your code here
 }
 
-function addManyNums10Timing(increment) {
+function addManyNums100Timing(increment) {
   // Copy your `addManyNums10` code here
   // Then, add timing code
 
   // Your code here
   let start = Date.now();
   let arr = [];
-  for (let i = increment; i <= 10 * increment; i += increment) {
-    console.time('addManyNums10Timing');
+  for (let i = increment; i <= 100 * increment; i += increment) {
+    console.time('addManyNums100Timing');
     arr.push(addManyNums(i));
-    console.timeLog('addManyNums10Timing');
-    console.timeEnd('addManyNums10Timing');
+    console.timeLog('addManyNums100Timing');
+    console.timeEnd('addManyNums100Timing');
   }
   let end = Date.now();
   console.log(end - start);
@@ -37,10 +37,10 @@ function addManyNums10Timing(increment) {
 
 n = 1000000;
 console.log(`addNums(${n}): `);
-addNums10Timing(1000000);
+addNums100Timing(1000000);
 
 console.log('\n***********\n');
 
 n = 1000;
 console.log(`addManyNums(${n}): `);
-addManyNums10Timing(5000);
+addManyNums100Timing(5000);
